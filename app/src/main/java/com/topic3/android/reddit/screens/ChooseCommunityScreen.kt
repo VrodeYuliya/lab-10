67bd63c5168bed2ef92d21b6d29ad85bd38d9ef3
VrodeYuliya <romanova.yulia5444@gmail.com>
2024-05-17 12:33:55 +0300

    Update ChooseCommunityScreen.kt

--- app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
@@ -1,19 +1,36 @@
 package com.topic3.android.reddit.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic3.android.reddit.R
+
 import com.topic3.android.reddit.routing.RedditRouter
 import com.topic3.android.reddit.viewmodel.MainViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+
 
 private const val SEARCH_DELAY_MILLIS = 300L
 
@@ -30,7 +47,15 @@ fun SearchedCommunities(
     viewModel: MainViewModel?,
     modifier: Modifier = Modifier
 ) {
-    //TODO Add your code here
+    communities.forEach{             Community(
+        text = it,
+        modifier = modifier,
+        onCommunityClicked = {
+            viewModel?.selectedCommunity?.postValue(it)
+            RedditRouter.goBack()
+        }
+    )
+    }
 }
 
 @Composable
@@ -63,4 +88,14 @@ fun ChooseCommunityTopBar(modifier: Modifier = Modifier) {
             .height(48.dp)
             .background(Color.Blue)
     )
+}
+@Preview
+@Composable
+fun SearchedCommunitiesPreview(){
+    Column{
+        SearchedCommunities(
+            defaultCommunities,
+            null,
+            Modifier)
+    }
 }
